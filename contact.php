3f1f38c4aa55ded990af0e15af0105ff0646e50e
Julia Morneau <morneau_julia@wheatoncollege.edu>
2014-12-04 13:55:38 -0500

    fixing up contact.php, still not working...

--- contact.php
+++ contact.php
@@ -29,43 +29,28 @@ if(isset($_POST['email'])) {
     $email_from = $_POST['email']; // required
     $comments = $_POST['comments']; // required
      
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-    $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+
     $email_message = "Form details below.\n\n";
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
      
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
      
-     echo 'Mail sent!';
-     
-// create email headers
-//$headers = 'From: '.$email_from."\r\n".
-//'Reply-To: '.$email_from."\r\n" .
-// 'X-Mailer: PHP/' . phpversion();
-// mail($email_to, $email_subject, $email_message, $headers);  
+ create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+$mail_status = mail($email_to, $email_subject, $email_message, $headers);  
+if ($mail_status) { ?>
+    <script language="javascript" type="text/javascript">
+        window.location = 'contact_page.html';
+    </script>
+}
+
+else { ?>
+    <script language="javascript" type="text/javascript">
+        window.location = 'contact_page.html';
+    </script>
 ?>
  
 <!-- place your own success html below -->
